494d25581e6da106e962934c8bac821499a824d3
Brownoak <helanii93@gmail.com>
2023-03-30 13:43:51 +0300

    added validation middleware

---
+++ server/middleware/validation/user.js
@@ -0,0 +1,24 @@
+// const {body} = require("express-validator")
+/**
+ *
+ * @param {String} type
+ *  LOGIN | REGISTER
+ */
+//    exports.validate = (type)=>{
+//     switch(type){
+//         case "REGISTER":
+//             return [
+//                 body("firstName").not().isEmpty().withMessage("First name required"),
+//                 body("lastName").not().isEmpty().withMessage("last name required"),
+//                 body("email").isEmail().withMessage("Invalid email address"),
+//                 body("password").not().isEmpty().isLength({min:6}).withMessage("password length must be 6")
+//             ];
+//         default:
+//             return[];
+//     }
+// }
+
+
+
+
+
